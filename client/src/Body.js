f27f8d81782991b13c41a892adbde568f99dd27e
CongThang1802 <congthangmyth@gmail.com>
2022-01-24 16:33:17 +0700

    add reset password

--- client/src/Body.js
+++ client/src/Body.js
@@ -1,40 +1,44 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-// import ForgotPassword from "./pages/forgotPassword";
-// import ResetPassword from "./pages/resetPassword";
+import Forgot from "./pages/forgotPassword";
+import Reset from "./pages/resetPassword";
 import NotFound from "./components/NotFound";
+import Alert from "./components/alert/Alert";
 
 function Body() {
   const auth = useSelector((state) => state.auth);
   const { isLogged } = auth;
   return (
-    <section>
-      <Switch>
-        <Route exact path="/" component={auth.token ? Home : Login} />
+    <Router>
+      <Alert />
+      <section>
+        <Switch>
+          <Route exact path="/" component={auth.token ? Home : Login} />
 
-        <Route
-          path="/register"
-          component={isLogged ? NotFound : Register}
-          exact
-        />
+          <Route
+            path="/register"
+            component={isLogged ? NotFound : Register}
+            exact
+          />
 
-        {/* <Route
-          path="/forgot_password"
-          component={isLogged ? NotFound : ForgotPassword}
-          exact
-        />
-        <Route
-          path="/reset/:token"
-          component={isLogged ? NotFound : ResetPassword}
-          exact
-        /> */}
-      </Switch>
-    </section>
+          <Route
+            path="/forgot_password"
+            component={isLogged ? NotFound : Forgot}
+            exact
+          />
+          <Route
+            path="/reset/:token"
+            component={isLogged ? NotFound : Reset}
+            exact
+          />
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
