d92ddf3472a9882148aeefabd426de02be7d18cb
CongThang1802 <congthangmyth@gmail.com>
2022-09-01 13:41:43 +0700

    fix upload image

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -23,11 +23,11 @@ export const imageUpload = async (images) => {
       formData.append("file", item);
     }
 
-    formData.append("upload_preset", "efxjficn");
-    formData.append("cloud_name", "devat-channel");
+    formData.append("upload_preset", "o5zvswi0");
+    formData.append("cloud_name", "eplus");
 
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/devat-channel/upload",
+      "https://api.cloudinary.com/v1_1/eplus/upload",
       {
         method: "POST",
         body: formData,
